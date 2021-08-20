c2455435dff62df7223ed7b725588270b2579200
Darshan Vekariya <darshanvekariya@Darshans-MacBook-Pro.local>
2021-08-20 02:46:37 -0400

    First demo selenium feature code

---
+++ src/page/LoginPage.java
@@ -0,0 +1,13 @@
+package page;
+
+public class LoginPage {
+	
+	public void loginPage1() {
+		System.out.println("Login Page");
+	}
+
+	public void loginSet1() {
+		System.out.println("Login Set");
+	}
+
+}
